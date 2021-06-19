1d82ca1a472aaf6d2efddae35fe3c529a90d552e
Jeff Buda <budajeff@gmail.com>
2021-06-19 07:54:51 -0400

    experience renders

--- src/ExperiencePage.js
+++ src/ExperiencePage.js
@@ -1,3 +1,18 @@
+import SiteData from './SiteData';
+import Card
+    from './Card';
 export default function ExperiencePage() {
-    return (<p>Experience</p>);
+    return (
+        <section>
+            <h2>Experience</h2>
+            {SiteData().experience.map(job =>
+                <Card
+                    title={job.title}
+                    subtitle={job.team}
+                    details={job.details}
+                >
+                </Card>
+            )}
+        </section>
+    );
 }
