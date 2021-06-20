25a1807fc75b35307cac39db058eda145ec974de
Jeff Buda <budajeff@gmail.com>
2021-06-20 11:25:00 -0400

    improved form impl

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -2,17 +2,23 @@ import React, { useState } from "react";
 import "./ContactForm.css";
 
 export default function ContactForm() {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [comments, setComments] = useState("");
+    const initialFormData = { name: '', emailAddress: '', comments: '' };
+    const [formData, setFormData] = useState(initialFormData);
     const [submittedOk, setSubmittedOk] = useState(undefined);
+
+    const handleFormChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const form = document.getElementById("contact-form");
         const data = new FormData(form);
         const response = await fetch(e.target.action, {
-            method: "POST",
+            method: e.target.method,
             body: data,
             headers: {
                 'Accept': 'application/json'
@@ -29,32 +35,35 @@ export default function ContactForm() {
     const form = (
         <div className="contact-form-container">
             <form id="contact-form" onSubmit={handleSubmit} action="https://formspree.io/f/mdoyzyjl" method="POST">
-                {/* <div className="input-labels">
+                <div className="input-labels">
                     <label labelFor="name">Name</label>
-                    <label labelFor="email">Email Address</label>
+                    <label labelFor="emailAddress">Email Address</label>
                     <label labelFor="comments">Comments</label>
-                </div> */}
+                </div>
 
                 <div className="input-fields">
                     <input
                         id="name"
                         name="name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
+                        required="true"
+                        value={formData.name}
+                        onChange={handleFormChange}
                     ></input>
 
                     <input
                         id="emailAddress"
                         name="emailAddress"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
+                        required="true"
+                        value={formData.emailAddress}
+                        onChange={handleFormChange}
                     ></input>
 
                     <textarea
                         id="comments"
                         name="comments"
-                        value={comments}
-                        onChange={(e) => setComments(e.target.value)}
+                        required="true"
+                        value={formData.comments}
+                        onChange={handleFormChange}
                     ></textarea>
 
                     <button id="submit-button" type="submit">Submit</button>
