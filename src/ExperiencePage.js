7938515d51222ee455ae88f5544ad48a0b07dc27
Jeff Buda <budajeff@gmail.com>
2021-06-20 20:13:05 -0400

    added dates to experience

--- src/ExperiencePage.js
+++ src/ExperiencePage.js
@@ -7,16 +7,20 @@ export default function ExperiencePage() {
         <section>
             <h2>Over 20 Years of Software Development Experience</h2>
             <Cards>
-                {SiteData().experience.map(job =>
-                    <Card
+                {SiteData().experience.map(job => 
+                {
+                    const endDate = job.endDate !== 'present' ? new Date(job.endDate).getFullYear() : 'Present';
+                    const startDate = new Date(job.startDate).getFullYear();
+                    const dateDetail = `${startDate} to ${endDate}`;
+                    return <Card
                         key={job.title + job.team}
                         title={job.title}
                         subtitle={job.team}
-                        details={job.details}
+                        details={[dateDetail, ...job.details]}
                         imageKey={job.image}
                     >
                     </Card>
-                )}
+})}
             </Cards>
 
         </section>
